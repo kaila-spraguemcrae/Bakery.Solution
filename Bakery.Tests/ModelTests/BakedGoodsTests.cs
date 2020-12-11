53e2b1a1e606c28bb3570aaec59da72a6a9b2689
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-11 09:33:08 -0800

    good fail for test 3 and refactor tests to include new parameter for Bread()

--- Bakery.Tests/ModelTests/BakedGoodsTests.cs
+++ Bakery.Tests/ModelTests/BakedGoodsTests.cs
@@ -9,20 +9,20 @@ namespace Bakery.Test
     [TestMethod]
     public void Bread_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
     [TestMethod]
     public void Bread_CreatesInstanceOfBreadPrice_Int()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(1);
       int result = newBread.Price;
       Assert.AreEqual(5, result);
     }
     [TestMethod]
     public void Bread_ReturnsQuantity_Int()
     {
-      Int quantity = 1
+      int quantity = 1;
       Bread newBread = new Bread(quantity);
       int result = newBread.Quantity;
       Assert.AreEqual(quantity, result);
--- Bakery/Models/BakedGoods.cs
+++ Bakery/Models/BakedGoods.cs
@@ -5,10 +5,12 @@ namespace Bakery.Models
   public class Bread
   {
     public int Price { get; set; }
+    public int Quantity { get; set; }
 
-    public Bread()
+    public Bread(int quantity)
     {
       Price = 5;
+      Quantity = 2;
     }
   }
 }
