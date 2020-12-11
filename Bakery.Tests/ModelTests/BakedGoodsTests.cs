bb9769277b5be9d323ed5d9706527f70ac13e514
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-11 08:59:00 -0800

    add test 2

--- Bakery.Tests/ModelTests/BakedGoodsTests.cs
+++ Bakery.Tests/ModelTests/BakedGoodsTests.cs
@@ -12,5 +12,12 @@ namespace Bakery.Test
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+    [TestMethod]
+    public void Bread_CreatesInstanceOfBreadPrice_Bread()
+    {
+      Bread newBread = new Bread();
+      int result = newBread.Price
+      Assert.AreEqual(5, result);
+    }
   }
 }
